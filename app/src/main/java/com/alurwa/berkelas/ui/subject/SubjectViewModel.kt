bf113bac4a01ea9b5a7f70f0ad0e849b47a00471
Purwa Sadr <purwasadr@gmail.com>
2021-09-20 13:02:41 +0700

    Add ViewModel for SubjectActivity

---
+++ app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectViewModel.kt
@@ -0,0 +1,12 @@
+package com.alurwa.berkelas.ui.subject
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SubjectViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
