8214de416dadfbb1c0bae46a3caba4111c752949
Purwa Sadr <purwasadr@gmail.com>
2021-09-20 12:43:09 +0700

    Add observeUser variable

--- app/src/main/java/com/alurwa/berkelas/ui/main/MainViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/main/MainViewModel.kt
@@ -1,16 +1,26 @@
 package com.alurwa.berkelas.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.alurwa.data.repository.auth.AuthRepository
+import com.alurwa.data.repository.auth.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val userRepository: UserRepository
 ) : ViewModel() {
     val isLogged = authRepository.isLogged()
+
+    val observeUser = userRepository
+        .observeUser()
+        .shareIn(viewModelScope, SharingStarted.WhileSubscribed(), 1)
+
     fun signOut() {
         authRepository.signOut()
     }
+
 }
