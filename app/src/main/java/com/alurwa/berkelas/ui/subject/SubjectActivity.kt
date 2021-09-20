4c55a84387a340b59750becd302d25a81dd0d481
Purwa Sadr <purwasadr@gmail.com>
2021-09-20 13:03:14 +0700

    Setup ViewPager and Adapter

--- app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectActivity.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectActivity.kt
@@ -2,17 +2,36 @@ package com.alurwa.berkelas.ui.subject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.alurwa.berkelas.adapter.SubjectVpAdapter
 import com.alurwa.berkelas.databinding.ActivitySubjectBinding
+import com.alurwa.berkelas.model.SubjectVpItem
+import com.alurwa.berkelas.util.setupToolbar
 
 class SubjectActivity : AppCompatActivity() {
     private val binding: ActivitySubjectBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivitySubjectBinding.inflate(layoutInflater)
     }
 
+    private val vpAdapter: SubjectVpAdapter by lazy(LazyThreadSafetyMode.NONE) {
+        SubjectVpAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.toolbar.setupToolbar(this, "Pelajaran", true)
 
+        setupVp()
+    }
+
+    private fun setupVp() {
+        binding.vpListSubject.offscreenPageLimit = 3
+        binding.vpListSubject.adapter = vpAdapter
+    }
+
+
+    private fun submitVpAdapter(vpList: List<SubjectVpItem>) {
+        vpAdapter.submitList(vpList)
     }
 }
