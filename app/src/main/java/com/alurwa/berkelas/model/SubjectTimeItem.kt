b0bb778106f3c2a727d7f7f559dfe21d5b45be6d
Purwa Sadr <purwasadr@gmail.com>
2021-09-20 12:49:29 +0700

    Add data class for subject

---
+++ app/src/main/java/com/alurwa/berkelas/model/SubjectTimeItem.kt
@@ -0,0 +1,7 @@
+package com.alurwa.berkelas.model
+
+data class SubjectTimeItem(
+    val subject: String,
+    val startTime: String,
+    val endTime: String
+)
---
+++ app/src/main/java/com/alurwa/berkelas/model/SubjectVpItem.kt
@@ -0,0 +1,6 @@
+package com.alurwa.berkelas.model
+
+data class SubjectVpItem(
+    val day: String,
+    val subjectTimeItem: SubjectTimeItem
+)
