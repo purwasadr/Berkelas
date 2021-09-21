37fcdf460069f5ddf9b3353c5f27ec2c754d3250
Purwa Sadr <purwasadr@gmail.com>
2021-09-21 13:56:02 +0700

    Add Model for subject

---
+++ common/src/main/java/com/alurwa/common/model/Subject.kt
@@ -0,0 +1,6 @@
+package com.alurwa.common.model
+
+data class Subject(
+    val day: Int,
+    val subjectItem: List<SubjectItem>
+)
---
+++ common/src/main/java/com/alurwa/common/model/SubjectItem.kt
@@ -0,0 +1,8 @@
+package com.alurwa.common.model
+
+data class SubjectItem(
+    val subject: String = "",
+    val startTime: String = "",
+    val endTime: String = "",
+    val teacher: String = ""
+)
