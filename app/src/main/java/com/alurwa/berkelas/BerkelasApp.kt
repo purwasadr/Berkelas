55da5a47952350812d76df27f371112a8fc0afb4
Purwa Sadr <purwasadr@gmail.com>
2021-09-21 13:55:22 +0700

    Set FirebaseFirestore setLoggingEnabled to true

--- app/src/main/java/com/alurwa/berkelas/BerkelasApp.kt
+++ app/src/main/java/com/alurwa/berkelas/BerkelasApp.kt
@@ -1,6 +1,7 @@
 package com.alurwa.berkelas
 
 import android.app.Application
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -10,9 +11,9 @@ class BerkelasApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+            FirebaseFirestore.setLoggingEnabled(true)
         }
     }
 }
