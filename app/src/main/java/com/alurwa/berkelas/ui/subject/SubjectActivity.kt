71b50a79de3184d0508ef2af424d0b33d884855f
Purwa Sadr <purwasadr@gmail.com>
2021-09-22 22:36:39 +0700

    Add observe function

--- app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectActivity.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectActivity.kt
@@ -1,11 +1,20 @@
 package com.alurwa.berkelas.ui.subject
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.alurwa.berkelas.adapter.SubjectVpAdapter
 import com.alurwa.berkelas.databinding.ActivitySubjectBinding
-import com.alurwa.berkelas.model.SubjectVpItem
 import com.alurwa.berkelas.util.setupToolbar
+import com.alurwa.common.model.Subject
+import com.alurwa.common.model.onError
+import com.alurwa.common.model.onLoading
+import com.alurwa.common.model.onSuccess
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class SubjectActivity : AppCompatActivity() {
     private val binding: ActivitySubjectBinding by lazy(LazyThreadSafetyMode.NONE) {
@@ -16,6 +25,8 @@ class SubjectActivity : AppCompatActivity() {
         SubjectVpAdapter()
     }
 
+    private val viewModel: SubjectViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -23,15 +34,39 @@ class SubjectActivity : AppCompatActivity() {
         binding.toolbar.setupToolbar(this, "Pelajaran", true)
 
         setupVp()
+
+        observeSubject()
     }
+
 
     private fun setupVp() {
         binding.vpListSubject.offscreenPageLimit = 3
         binding.vpListSubject.adapter = vpAdapter
     }
 
+    private fun observeSubject() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.observeSubject.collectLatest {
+                    it.onSuccess { data ->
+                        submitVpAdapter(data)
+                    }.onLoading {
 
-    private fun submitVpAdapter(vpList: List<SubjectVpItem>) {
+                    }.onError {
+
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun submitVpAdapter(vpList: List<Subject>) {
         vpAdapter.submitList(vpList)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
