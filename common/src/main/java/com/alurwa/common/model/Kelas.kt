c503cfd9eb88d3fdbe9e1f417833d699e370babb
Purwa Sadr <purwasadr@gmail.com>
2021-09-22 22:26:00 +0700

    Add Kelas data class

---
+++ common/src/main/java/com/alurwa/common/model/Kelas.kt
@@ -0,0 +1,13 @@
+package com.alurwa.common.model
+
+data class Kelas(
+    val id: String,
+    val name: String,
+) {
+    companion object {
+        val EMPTY = Kelas(
+            id = "",
+            name = ""
+        )
+    }
+}
