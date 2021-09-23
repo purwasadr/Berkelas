742f59e13f3eb7934e8916d54a56c50c198c4b38
Purwa Sadr <purwasadr@gmail.com>
2021-09-23 22:40:55 +0700

    Add default value

--- common/src/main/java/com/alurwa/common/model/Kelas.kt
+++ common/src/main/java/com/alurwa/common/model/Kelas.kt
@@ -1,8 +1,8 @@
 package com.alurwa.common.model
 
 data class Kelas(
-    val id: String,
-    val name: String,
+    val id: String = "",
+    val name: String = "",
 ) {
     companion object {
         val EMPTY = Kelas(
