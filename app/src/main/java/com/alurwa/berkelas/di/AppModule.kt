fecd2ee80ffce8a41e3a3e5f401c701b9e613fc4
Purwa Sadr <purwasadr@gmail.com>
2021-09-23 22:40:04 +0700

    provide SessionSharedPreferences in Hilt Module

--- app/src/main/java/com/alurwa/berkelas/di/AppModule.kt
+++ app/src/main/java/com/alurwa/berkelas/di/AppModule.kt
@@ -1,9 +1,13 @@
 package com.alurwa.berkelas.di
 
+import android.content.Context
+import android.content.SharedPreferences
 import com.alurwa.common.di.DispatcherIO
+import com.alurwa.common.di.SessionPreferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Singleton
@@ -17,4 +21,14 @@ object AppModule {
     @Singleton
     @Provides
     fun provideDispatcherIO() = Dispatchers.IO
+
+    @SessionPreferences
+    @Singleton
+    @Provides
+    fun provideSessionSharedPreferences(
+        @ApplicationContext context: Context
+    ): SharedPreferences = context.getSharedPreferences(
+        "com.alurwa.berkelas.session_user",
+        Context.MODE_PRIVATE
+    )
 }
