529b12c4d73aa9863e4ca34369d6cbc95ee3ee6f
Purwa Sadr <purwasadr@gmail.com>
2021-09-24 12:50:47 +0700

    Add SessionPreferences annotation class

--- common/src/main/java/com/alurwa/common/di/Annotations.kt
+++ common/src/main/java/com/alurwa/common/di/Annotations.kt
@@ -5,3 +5,7 @@ import javax.inject.Qualifier
 @Qualifier
 @Retention(AnnotationRetention.RUNTIME)
 annotation class DispatcherIO
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class SessionPreferences
