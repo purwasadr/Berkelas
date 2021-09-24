47db0cd01907b5985d7d7f3befca4c61d6072492
Purwa Sadr <purwasadr@gmail.com>
2021-09-24 21:04:50 +0700

    Add function to add and edit subject

--- app/src/main/java/com/alurwa/berkelas/ui/addeditsubject/AddEditSubjectViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/addeditsubject/AddEditSubjectViewModel.kt
@@ -1,13 +1,23 @@
 package com.alurwa.berkelas.ui.addeditsubject
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.alurwa.common.model.SubjectItem
 import com.alurwa.data.repository.subject.SubjectRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class AddEditSubjectViewModel @Inject constructor(
-    private val subjectRepository: SubjectRepository
+    private val subjectRepository: SubjectRepository,
+    private val stateHandle: SavedStateHandle
 ) : ViewModel() {
+    val mode = stateHandle.get<Int>(AddEditSubjectActivity.EXTRA_MODE)!!
+    val subjectItem = stateHandle.get<SubjectItem?>("Ss")
+    val day = stateHandle.get<Int>(AddEditSubjectActivity.EXTRA_DAY)!!
 
+
+    fun addSubject(day: Int, subject: SubjectItem) = subjectRepository.addSubject(day, subject)
+
+    fun editSubject(day: Int, subject: SubjectItem) = subjectRepository.editSubject(day, subject)
 }
