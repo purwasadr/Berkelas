2c80595c1efefbaa69bed83bc230587dabace7c8
Purwa Sadr <purwasadr@gmail.com>
2021-09-24 21:53:55 +0700

    Add stateflow for day

--- app/src/main/java/com/alurwa/berkelas/ui/addeditsubject/AddEditSubjectViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/addeditsubject/AddEditSubjectViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel
 import com.alurwa.common.model.SubjectItem
 import com.alurwa.data.repository.subject.SubjectRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,12 +14,22 @@ class AddEditSubjectViewModel @Inject constructor(
     private val subjectRepository: SubjectRepository,
     private val stateHandle: SavedStateHandle
 ) : ViewModel() {
-    val mode = stateHandle.get<Int>(AddEditSubjectActivity.EXTRA_MODE)!!
-    val subjectItem = stateHandle.get<SubjectItem?>("Ss")
-    val day = stateHandle.get<Int>(AddEditSubjectActivity.EXTRA_DAY)!!
 
+    val mode = stateHandle.get<Int>(AddEditSubjectActivity.EXTRA_MODE)!!
+
+    val subjectItem = stateHandle.get<SubjectItem?>("Ss")
+
+    private val _day = MutableStateFlow<Int>(
+        stateHandle.get<Int>(AddEditSubjectActivity.EXTRA_DAY)!!
+    )
+
+    val day = _day.asStateFlow()
 
     fun addSubject(day: Int, subject: SubjectItem) = subjectRepository.addSubject(day, subject)
 
     fun editSubject(day: Int, subject: SubjectItem) = subjectRepository.editSubject(day, subject)
+
+    fun setDay(day: Int) {
+        _day.value = day
+    }
 }
