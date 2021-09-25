ccb2f591961c5cbb3caa82462746c5c5ed9ec1d5
Purwa Sadr <purwasadr@gmail.com>
2021-09-25 23:09:37 +0700

    Add many function

--- app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectActivity.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectActivity.kt
@@ -1,40 +1,51 @@
 package com.alurwa.berkelas.ui.subject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import com.alurwa.berkelas.R
 import com.alurwa.berkelas.adapter.SubjectVpAdapter
 import com.alurwa.berkelas.databinding.ActivitySubjectBinding
+import com.alurwa.berkelas.ui.addeditsubject.AddEditSubjectActivity
+import com.alurwa.berkelas.util.SnackbarUtil
 import com.alurwa.berkelas.util.setupToolbar
-import com.alurwa.common.model.Subject
-import com.alurwa.common.model.onError
-import com.alurwa.common.model.onLoading
-import com.alurwa.common.model.onSuccess
+import com.alurwa.common.model.*
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
+@AndroidEntryPoint
 class SubjectActivity : AppCompatActivity() {
     private val binding: ActivitySubjectBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivitySubjectBinding.inflate(layoutInflater)
     }
 
     private val vpAdapter: SubjectVpAdapter by lazy(LazyThreadSafetyMode.NONE) {
-        SubjectVpAdapter()
+        SubjectVpAdapter() {
+            showSubjectInfo(it)
+        }
     }
 
     private val viewModel: SubjectViewModel by viewModels()
+
+    private val currentItem get() = binding.vpListSubject.currentItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.toolbar.setupToolbar(this, "Pelajaran", true)
+        binding.toolbar.setupToolbar(
+            this,
+            getString(R.string.toolbar_title_subject),
+            true
+        )
 
         setupVp()
-
+        setupFab()
         observeSubject()
     }
 
@@ -44,25 +55,74 @@ class SubjectActivity : AppCompatActivity() {
         binding.vpListSubject.adapter = vpAdapter
     }
 
+    private fun setupFab() {
+        binding.fab.setOnClickListener {
+            navigateToAddSubject()
+        }
+    }
+
     private fun observeSubject() {
         lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.observeSubject.collectLatest {
-                    it.onSuccess { data ->
+
+            // FIXME: List Adapter not show list correctly when using repeatOnLifecycle
+            viewModel.observeSubject
+                .distinctUntilChanged()
+                .collectLatest { result ->
+                    result.onSuccess { data ->
                         submitVpAdapter(data)
+                        Timber.d(data.get(2).toString())
                     }.onLoading {
 
                     }.onError {
-
+                        SnackbarUtil.showShort(binding.root, it.message)
                     }
                 }
-            }
         }
     }
 
 
     private fun submitVpAdapter(vpList: List<Subject>) {
+
+        val transform = List(7) {
+            vpList.find { find ->
+                find.day == it
+            } ?: Subject(
+                day = it,
+                subjectItem = emptyList()
+            )
+        }
+
         vpAdapter.submitList(vpList)
+    }
+
+    private fun showSubjectInfo(subjectItem: SubjectItem) {
+        SubjectInfoDialog(this, subjectItem)
+            .setOnClickBtnEdit {
+                navigateToEditSubject(subjectItem)
+            }
+            .setOnClickBtnDelete {
+
+            }
+            .show()
+    }
+
+    private fun navigateToAddSubject() {
+        Intent(this, AddEditSubjectActivity::class.java)
+            .putExtra(AddEditSubjectActivity.EXTRA_DAY, binding.vpListSubject.currentItem)
+            .putExtra(AddEditSubjectActivity.EXTRA_MODE, AddEditSubjectActivity.MODE_ADD)
+            .also {
+                startActivity(it)
+            }
+    }
+
+    private fun navigateToEditSubject(subjectItem: SubjectItem) {
+        Intent(this, AddEditSubjectActivity::class.java)
+            .putExtra(AddEditSubjectActivity.EXTRA_DAY, binding.vpListSubject.currentItem)
+            .putExtra(AddEditSubjectActivity.EXTRA_MODE, AddEditSubjectActivity.MODE_EDIT)
+            .putExtra(AddEditSubjectActivity.EXTRA_SUBJECT, subjectItem)
+            .also {
+                startActivity(it)
+            }
     }
 
     override fun onSupportNavigateUp(): Boolean {
