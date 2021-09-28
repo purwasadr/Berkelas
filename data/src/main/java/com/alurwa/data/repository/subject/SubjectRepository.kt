841c557e1c05193731a473cc7c32a56a93c95c18
Purwa Sadr <purwasadr@gmail.com>
2021-09-28 07:53:56 +0700

    Remove unnecessary code

--- data/src/main/java/com/alurwa/data/repository/subject/SubjectRepository.kt
+++ data/src/main/java/com/alurwa/data/repository/subject/SubjectRepository.kt
@@ -1,50 +1,103 @@
 package com.alurwa.data.repository.subject
 
+import com.alurwa.common.di.DispatcherIO
+import com.alurwa.common.extension.catchToResult
 import com.alurwa.common.model.Result
 import com.alurwa.common.model.Subject
 import com.alurwa.common.model.SubjectItem
+import com.alurwa.data.firebase.listener
+import com.alurwa.data.firebase.subjectSharedCol
+import com.alurwa.data.util.SessionManager
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class SubjectRepository @Inject constructor(
     private val firestore: FirebaseFirestore,
+    private val sessionManager: SessionManager,
+    @DispatcherIO
+    private val dispatcher: CoroutineDispatcher
 ) {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     fun observeSubject() = callbackFlow<Result<List<Subject>>> {
         trySend(Result.Loading)
 
-        val listSubject = listOf(
-            Subject(
-                day = 0,
-                subjectItem =  listOf(
-                    SubjectItem(
-                        subject = "Matematika", startTime = "09:20",
-                        endTime = "10:20",
-                        teacher = "Dramaturgy"
+        val school = sessionManager.getUserSchool()
 
-                    )
-                ),
-            ),
-            Subject(
-                day = 1,
-                subjectItem =  listOf(
-                    SubjectItem(
-                        subject = "Geografi",
-                        startTime = "11:20",
-                        endTime = "12:20",
-                        teacher = "Episode"
-                    )
-                ),
-            )
-        )
+        val listener = firestore.subjectSharedCol(school.schoolId, school.kelasId)
+            .listener(Subject::class.java) {
+                trySendBlocking(it)
+            }
 
-        trySend(Result.Success(listSubject))
         awaitClose {
-
+            listener.remove()
         }
+    }.catchToResult()
+
+    fun addSubject(day: Int, subjectItem: SubjectItem) = flow {
+        emit(Result.Loading)
+
+        val school = sessionManager.getUserSchool()
+        val ref = firestore.subjectSharedCol(school.schoolId, school.kelasId)
+            .document(day.toString())
+
+        firestore.runTransaction {
+            val subjectResult = it.get(ref)
+                .toObject(Subject::class.java)
+
+            val subjectItemResult = subjectResult?.subjectItem ?: emptyList()
+
+            it.set(
+                ref,
+                Subject(
+                    day = day, subjectItem = subjectItemResult.plus(subjectItem)
+                )
+            )
+        }.await()
+
+        emit(Result.Success(true))
+    }.catchToResult().flowOn(dispatcher)
+
+    fun editSubject(day: Int, subjectItem: SubjectItem) = flow {
+        emit(Result.Loading)
+
+        val school = sessionManager.getUserSchool()
+
+        val ref = firestore.subjectSharedCol(school.schoolId, school.kelasId)
+            .document(day.toString())
+
+        firestore.runTransaction {
+            val subjectResult = it.get(ref)
+                .toObject(Subject::class.java)
+
+            val subjectItemResult = subjectResult?.subjectItem!!
+
+            it.set(
+                ref,
+                Subject(
+                    day = day, subjectItem = subjectItemResult.map { item ->
+                        if (subjectItem.id == item.id) {
+                            subjectItem
+                        } else {
+                            item
+                        }
+                    }
+                )
+            )
+        }.await()
+
+        emit(Result.Success(true))
+    }.catchToResult().flowOn(dispatcher)
+
+    fun deleteSubjectItem(day: Int, id: String) {
+
     }
 }
