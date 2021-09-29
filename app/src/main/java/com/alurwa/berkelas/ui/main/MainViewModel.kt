be0791324d15e1ea1840ad80963f4685f54f07f1
Purwa Sadr <purwasadr@gmail.com>
2021-09-29 16:50:38 +0700

    Add user variable and method

--- app/src/main/java/com/alurwa/berkelas/ui/main/MainViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/main/MainViewModel.kt
@@ -1,7 +1,10 @@
 package com.alurwa.berkelas.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.alurwa.common.model.User
 import com.alurwa.data.repository.auth.AuthRepository
 import com.alurwa.data.repository.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -24,4 +27,10 @@ class MainViewModel @Inject constructor(
         authRepository.signOut()
     }
 
+    private val _user = MutableLiveData<User>()
+    val user: LiveData<User> = _user
+
+    fun setUser(user: User) {
+        _user.value = user
+    }
 }
