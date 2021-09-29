5bad3cfa0f0c9889b51d86bf4ad4b43876677313
Purwa Sadr <purwasadr@gmail.com>
2021-09-29 16:26:29 +0700

    Add mode and room variable, and addRoom and editRoom method

--- app/src/main/java/com/alurwa/berkelas/ui/roomaddedit/RoomAddEditViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/roomaddedit/RoomAddEditViewModel.kt
@@ -1,14 +1,29 @@
 package com.alurwa.berkelas.ui.roomaddedit
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.alurwa.common.model.RoomData
 import com.alurwa.data.repository.room.RoomRepository
 import com.alurwa.data.repository.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class RoomAddEditViewModel constructor(
+class RoomAddEditViewModel @Inject constructor(
     private val userRepository: UserRepository,
-    private val roomRepository: RoomRepository
+    private val roomRepository: RoomRepository,
+    private val stateHandle: SavedStateHandle
 ) : ViewModel() {
 
+    val mode = stateHandle.get<Int>(RoomAddEditActivity.EXTRA_MODE)!!
+
+    val room = if (mode == RoomAddEditActivity.MODE_EDIT) {
+        stateHandle.get<RoomData>(RoomAddEditActivity.EXTRA_ROOM)!!
+    } else {
+        null
+    }
+
+    fun addRoom(roomData: RoomData) = roomRepository.addRoomData(roomData)
+
+    fun editRoom(roomData: RoomData) = roomRepository.ediRoomData(roomData)
 }
