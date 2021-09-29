c4f9a6ee75c30dbf53d84a13ba551e651e628880
Purwa Sadr <purwasadr@gmail.com>
2021-09-29 16:41:52 +0700

    Add observeMyUser, mapUserToUserWithoutSchool, fillUserData, navigateTo method

--- app/src/main/java/com/alurwa/berkelas/ui/account/AccountFragment.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/account/AccountFragment.kt
@@ -1,15 +1,28 @@
 package com.alurwa.berkelas.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.alurwa.berkelas.adapter.AccountMenuAdapter
 import com.alurwa.berkelas.databinding.FragmentAccountBinding
 import com.alurwa.berkelas.model.MainMenuItem
+import com.alurwa.berkelas.ui.accountedit.AccountEditActivity
+import com.alurwa.berkelas.ui.main.MainViewModel
+import com.alurwa.berkelas.ui.roomlist.RoomListActivity
+import com.alurwa.common.model.User
+import com.alurwa.common.model.UserWithoutRoom
+import com.alurwa.common.model.onSuccess
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class AccountFragment : Fragment() {
@@ -18,9 +31,11 @@ class AccountFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+    private val viewModel: MainViewModel by activityViewModels()
+
     private val accountMenuAdapter: AccountMenuAdapter by lazy(LazyThreadSafetyMode.NONE) {
         AccountMenuAdapter {
-
+            navigateTo(it)
         }
     }
 
@@ -39,7 +54,42 @@ class AccountFragment : Fragment() {
         setupAccountMenu()
 
         fillAccountMenuAdapter()
+
+        observeMyUser()
     }
+
+    private fun observeMyUser() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.observeUser.collectLatest { result ->
+                    result.onSuccess {
+                        val data = it
+                        if (data != null) {
+                            fillUserData(data)
+
+                            viewModel.setUser(
+                                data
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun mapUserToUserWithoutSchool(user: User) =
+        with(user) {
+            UserWithoutRoom(
+                uid = uid,
+                email = email,
+                profileImgUrl = profileImgUrl,
+                username = username,
+                fullName = fullName,
+                nickname = nickname,
+                dateOfBirth = dateOfBirth,
+                gender = gender
+            )
+        }
 
     private fun setupAccountMenu() {
         with(binding) {
@@ -49,10 +99,21 @@ class AccountFragment : Fragment() {
         }
     }
 
+    private fun fillUserData(user: User) {
+        with(binding) {
+            txtEmail.text = user.email
+            txtUsername.text = user.username
+        }
+    }
+
     private fun fillAccountMenuAdapter() {
         val accountMenuList = listOf(
             MainMenuItem(
-                "Akun Saya",
+                "Edit Akun",
+                true
+            ),
+            MainMenuItem(
+                "Ganti Room",
                 true
             ),
             MainMenuItem(
@@ -62,5 +123,29 @@ class AccountFragment : Fragment() {
         )
 
         accountMenuAdapter.submitList(accountMenuList)
+    }
+
+    private fun navigateTo(position: Int) {
+        when (position) {
+            0 -> {
+                val user = viewModel.user.value
+                val userWS = mapUserToUserWithoutSchool(user ?: User.EMPTY)
+
+                Intent(requireContext(), AccountEditActivity::class.java)
+                    .putExtra(AccountEditActivity.EXTRA_USER_WITHOUT_SCHOOL, userWS)
+                    .also {
+                        startActivity(it)
+                    }
+            }
+
+            1 -> {
+                val roomId = viewModel.user.value?.roomId ?: ""
+
+                Intent(requireContext(), RoomListActivity::class.java)
+                    .also {
+                        startActivity(it)
+                    }
+            }
+        }
     }
 }
