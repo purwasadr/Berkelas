4b02c388bdeda7a921b5e705b0e1b57506cacd6b
Purwa Sadr <purwasadr@gmail.com>
2021-09-30 17:36:36 +0700

    Add extension function

--- app/src/main/java/com/alurwa/berkelas/extension/TextInputLayoutExtension.kt
+++ app/src/main/java/com/alurwa/berkelas/extension/TextInputLayoutExtension.kt
@@ -1,10 +1,17 @@
 package com.alurwa.berkelas.extension
 
+import androidx.annotation.StringRes
 import com.google.android.material.textfield.TextInputLayout
 
 fun TextInputLayout.showError(text: String) {
     this.isErrorEnabled = true
     this.error = text
+}
+
+
+fun TextInputLayout.showError(@StringRes resId: Int) {
+    this.isErrorEnabled = true
+    this.error = this.context.getString(resId)
 }
 
 fun TextInputLayout.removeError() {
