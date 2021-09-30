b18b1634d5f00394ca721967744ca4fedc09e827
Purwa Sadr <purwasadr@gmail.com>
2021-09-30 20:56:52 +0700

    Add subject collection ref and room collection ref

--- data/src/main/java/com/alurwa/data/firebase/FirebaseRef.kt
+++ data/src/main/java/com/alurwa/data/firebase/FirebaseRef.kt
@@ -5,3 +5,19 @@ import com.google.firebase.firestore.FirebaseFirestore
 fun FirebaseFirestore.userCol() = collection("users")
 
 fun FirebaseFirestore.userDoc(id: String) = userCol().document(id)
+
+fun FirebaseFirestore.subjectCol() = collection("subject")
+
+private fun FirebaseFirestore.subjectSharedDoc(roomId: String) =
+    subjectCol()
+        .document("shared")
+        .collection("room")
+        .document(roomId)
+
+fun FirebaseFirestore.subjectSharedCol(roomId: String) =
+    subjectSharedDoc(roomId)
+        .collection("subjectData")
+
+fun FirebaseFirestore.roomCol() = collection("room")
+
+fun FirebaseFirestore.roomDoc(id: String) = roomCol().document(id)
