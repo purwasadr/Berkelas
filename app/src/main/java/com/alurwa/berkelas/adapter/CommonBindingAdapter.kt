3d9f2f1dc7b87b16bd7a7299a101bb93dba5d612
Purwa Sadr <purwasadr@gmail.com>
2021-10-02 17:42:46 +0700

    Add binding adapter txtDay() for showing day from int

--- app/src/main/java/com/alurwa/berkelas/adapter/CommonBindingAdapter.kt
+++ app/src/main/java/com/alurwa/berkelas/adapter/CommonBindingAdapter.kt
@@ -3,6 +3,7 @@ package com.alurwa.berkelas.adapter
 import android.widget.AutoCompleteTextView
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.alurwa.berkelas.R
 import com.alurwa.berkelas.util.Gender
 import com.bumptech.glide.Glide
 import java.text.SimpleDateFormat
@@ -36,5 +37,13 @@ object CommonBindingAdapter {
         Glide.with(view.context)
             .load(url)
             .into(view)
+    }
+
+    @JvmStatic
+    @BindingAdapter("actDay")
+    fun actDay(view: AutoCompleteTextView, day: Int) {
+        val dayString = view.context.resources.getStringArray(R.array.day_of_week)
+
+        view.setText(dayString.getOrNull(day))
     }
 }
