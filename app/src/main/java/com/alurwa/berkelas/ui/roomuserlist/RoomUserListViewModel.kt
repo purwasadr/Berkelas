0e98d906b1ee1bee3db4f69ef2a75582ce5af45c
Purwa Sadr <purwasadr@gmail.com>
2021-10-03 11:44:22 +0700

    Add userRepository inject and add users variable to get observeUsersByRoomId from UserRepository

--- app/src/main/java/com/alurwa/berkelas/ui/roomuserlist/RoomUserListViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/roomuserlist/RoomUserListViewModel.kt
@@ -1,7 +1,14 @@
 package com.alurwa.berkelas.ui.roomuserlist
 
 import androidx.lifecycle.ViewModel
+import com.alurwa.data.repository.user.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class RoomUserListViewModel : ViewModel() {
+@HiltViewModel
+class RoomUserListViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
 
+    val users = userRepository.observeUsersByRoomId()
 }
