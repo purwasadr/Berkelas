0539c75dcc07d0a697ae72b6d9beea51bb012950
Purwa Sadr <purwasadr@gmail.com>
2021-10-04 20:31:12 +0700

    Remove unused code and change loginWithEmail return type

--- data/src/main/java/com/alurwa/data/repository/auth/AuthRepository.kt
+++ data/src/main/java/com/alurwa/data/repository/auth/AuthRepository.kt
@@ -1,21 +1,17 @@
 package com.alurwa.data.repository.auth
 
 import com.alurwa.common.di.DispatcherIO
+import com.alurwa.common.extension.catchToResult
 import com.alurwa.common.model.Result
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class AuthRepository @Inject constructor(
@@ -27,66 +23,37 @@ class AuthRepository @Inject constructor(
     fun loginWithEmail(
         email: String,
         password: String
-    ): Flow<Result<Boolean>> = flow {
+    ): Flow<Result<FirebaseUser>> = flow {
         emit(Result.Loading)
 
         auth.signInWithEmailAndPassword(
             email,
             password
         ).await().also {
-                if (it.user != null) emit(Result.Success(true))
+            val user = it.user
+                if (user != null) emit(Result.Success(user))
                 else throw Exception("Login Failure")
             }
 
-
-//        val processAuth = suspendCoroutine<Result<Boolean>> {
-//            auth.signInWithEmailAndPassword(
-//                email,
-//                password
-//            ).addOnCompleteListener { task: Task<AuthResult?> ->
-//                if (!task.isSuccessful) {
-//                    it.resume(Result.Error(task.exception ?: Exception()))
-//                } else {
-//                    it.resume(Result.Success(true))
-//                }
-//            }
-//        }
-
-        //  emit(processAuth)
-    }.catch {
-        emit(Result.Error(Exception(it)))
-    }.flowOn(dispatcherIO)
+    }.catchToResult().flowOn(dispatcherIO)
 
     fun signUpWithEmail(
         email: String,
         password: String
-    ): Flow<Result<Boolean>> = flow {
+    ) = flow {
         emit(Result.Loading)
-
-//        suspendCoroutine<Result<Boolean>> {
-//            auth.createUserWithEmailAndPassword(
-//                email,
-//                password
-//            ).addOnCompleteListener { task: Task<AuthResult?> ->
-//                if (!task.isSuccessful) {
-//                    it.resume(Result.Error(task.exception ?: Exception()))
-//                } else {
-//                    it.resume(Result.Success(true))
-//                }
-//            }
-//        }.also {
-//            emit(it)
-//        }
 
         auth.createUserWithEmailAndPassword(
             email,
             password
-        ).await()
+        ).await().also {
+            val user = it.user
 
-        emit(Result.Success(true))
-    }.catch {
-        emit(Result.Error(Exception(it)))
-    }.flowOn(dispatcherIO)
+            if (user != null) emit(Result.Success(user))
+            else throw Exception("Login Failure")
+        }
+
+    }.catchToResult().flowOn(dispatcherIO)
 
     fun isLogged() = auth.currentUser != null
 
