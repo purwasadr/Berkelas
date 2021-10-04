98e0dc8eb9c26327b63222d2d77d3dbc8769c32f
Purwa Sadr <purwasadr@gmail.com>
2021-10-04 20:24:59 +0700

    Change to sealed class

--- app/src/main/java/com/alurwa/berkelas/model/CashAllItem.kt
+++ app/src/main/java/com/alurwa/berkelas/model/CashAllItem.kt
@@ -1,7 +1,16 @@
 package com.alurwa.berkelas.model
 
-data class CashAllItem(
-    val id: String,
-    val name: String,
-    val amount: Int,
-)
+sealed class CashAllItem {
+    data class Cash(
+        val id: String,
+        val date: String,
+        val amount: Int,
+        val count: Int,
+    ) : CashAllItem()
+
+    data class People(
+        val id: String,
+        val name: String,
+        val amount: Int,
+    ) : CashAllItem()
+}
