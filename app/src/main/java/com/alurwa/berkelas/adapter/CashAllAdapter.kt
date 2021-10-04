95fe849e99f5136c39ee58bf2a44188a3eed9676
Purwa Sadr <purwasadr@gmail.com>
2021-10-04 20:20:01 +0700

    Add multiple viewType

--- app/src/main/java/com/alurwa/berkelas/adapter/CashAllAdapter.kt
+++ app/src/main/java/com/alurwa/berkelas/adapter/CashAllAdapter.kt
@@ -5,38 +5,72 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.alurwa.berkelas.R
 import com.alurwa.berkelas.databinding.ListItemCashAllBinding
+import com.alurwa.berkelas.databinding.ListItemCashAllDateBinding
 import com.alurwa.berkelas.model.CashAllItem
 
-class CashAllAdapter (
+class CashAllAdapter(
     private val onItemClick: (position: Int) -> Unit
-) : ListAdapter<CashAllItem, CashAllAdapter.ViewHolder>(COMPARATOR) {
+) : ListAdapter<CashAllItem, RecyclerView.ViewHolder>(COMPARATOR) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(
-            ListItemCashAllBinding.inflate(
-                LayoutInflater.from(
-                    parent.context
-                ), parent, false
-            )
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            R.layout.list_item_cash_all_date -> {
+                ListItemCashAllDateBinding.inflate(
+                    layoutInflater, parent, false
+                ).let {
+                    CashViewHolder(it)
+                }
+            }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(position)
+            else -> {
+                ListItemCashAllBinding.inflate(
+                    layoutInflater, parent, false
+                ).let {
+                    PeopleViewHolder(it)
+                }
+            }
+        }
+
     }
 
-    inner class ViewHolder(
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = getItem(position)
+        when (holder) {
+            is CashViewHolder -> holder.bind(item as CashAllItem.Cash)
+            is PeopleViewHolder -> holder.bind(item as CashAllItem.People)
+        }
+    }
+
+    override fun getItemViewType(position: Int) = when (getItem(position)) {
+        is CashAllItem.Cash -> R.layout.list_item_cash_all_date
+        is CashAllItem.People -> R.layout.list_item_cash_all
+        else -> throw IllegalStateException("Unknown view")
+    }
+
+    inner class PeopleViewHolder(
         private val binding: ListItemCashAllBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(position: Int) {
+        fun bind(item: CashAllItem.People) {
             with(binding) {
-                val data = getItem(position)
-                txtName.text = data.name
-                txtCount.text = data.amount.toString()
-                root.setOnClickListener {
-                    onItemClick(position)
-                }
+                txtName.text = item.name
+                txtCount.text = item.amount.toString()
+            }
+
+        }
+    }
+
+    inner class CashViewHolder(
+        private val binding: ListItemCashAllDateBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: CashAllItem.Cash) {
+            with(binding) {
+
             }
 
         }
@@ -45,12 +79,22 @@ class CashAllAdapter (
     companion object {
 
         val COMPARATOR = object : DiffUtil.ItemCallback<CashAllItem>() {
-            override fun areItemsTheSame(oldItem: CashAllItem, newItem: CashAllItem): Boolean =
-                oldItem.id  == newItem.id
+            override fun areItemsTheSame(oldItem: CashAllItem, newItem: CashAllItem): Boolean {
+                val item1 =
+                    oldItem is CashAllItem.Cash && newItem is CashAllItem.Cash && oldItem.id == newItem.id
+
+                val item2 =
+                    oldItem is CashAllItem.People && newItem is CashAllItem.People && oldItem.id == newItem.id
+
+                return item1 || item2
+            }
 
             override fun areContentsTheSame(oldItem: CashAllItem, newItem: CashAllItem): Boolean =
                 oldItem == newItem
 
         }
+
+        const val TYPE_AS_CASH = 100
+        const val TYPE_AS_PEOPLE = 101
     }
 }
