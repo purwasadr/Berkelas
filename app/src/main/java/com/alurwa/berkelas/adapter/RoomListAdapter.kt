8edc4f261e4c78d4a19fdaae23c64b1bddf5249d
Purwa Sadr <purwasadr@gmail.com>
2021-10-04 20:22:00 +0700

    Add variable to get roomData

--- app/src/main/java/com/alurwa/berkelas/adapter/RoomListAdapter.kt
+++ app/src/main/java/com/alurwa/berkelas/adapter/RoomListAdapter.kt
@@ -2,15 +2,17 @@ package com.alurwa.berkelas.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.alurwa.berkelas.databinding.ListItemRoomBinding
+import com.alurwa.berkelas.model.RoomItem
 import com.alurwa.common.model.RoomData
 
 class RoomListAdapter(
     val onItemClick: (roomData: RoomData) -> Unit
-) : ListAdapter<RoomData, RoomListAdapter.ViewHolder>(COMPARATOR) {
+) : ListAdapter<RoomItem, RoomListAdapter.ViewHolder>(COMPARATOR) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder(
@@ -30,23 +32,25 @@ class RoomListAdapter(
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(position: Int) {
-            val data = getItem(position) ?: RoomData.EMPTY
+            val data = getItem(position)
+            val roomData = data.roomData
 
-            binding.txtName.text = data.roomName
-            binding.txtSchool.text = data.schoolName
+            binding.txtName.text = roomData.roomName
+            binding.txtSchool.text = roomData.schoolName
+            binding.imgCheck.isVisible = data.checked
             binding.root.setOnClickListener {
-                onItemClick(data)
+                onItemClick(roomData)
             }
         }
     }
 
     companion object {
 
-        val COMPARATOR = object : DiffUtil.ItemCallback<RoomData>() {
-            override fun areItemsTheSame(oldItem: RoomData, newItem: RoomData): Boolean =
-                oldItem == newItem
+        val COMPARATOR = object : DiffUtil.ItemCallback<RoomItem>() {
+            override fun areItemsTheSame(oldItem: RoomItem, newItem: RoomItem): Boolean =
+                oldItem.roomData.id == newItem.roomData.id
 
-            override fun areContentsTheSame(oldItem: RoomData, newItem: RoomData): Boolean =
+            override fun areContentsTheSame(oldItem: RoomItem, newItem: RoomItem): Boolean =
                 oldItem == newItem
 
         }
