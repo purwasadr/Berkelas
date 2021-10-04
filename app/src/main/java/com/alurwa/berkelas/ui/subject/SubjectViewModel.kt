a5caf81b447808bc240aba384f8083165cf0c37d
Purwa Sadr <purwasadr@gmail.com>
2021-10-04 21:01:38 +0700

    Remove unused code

--- app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectViewModel.kt
@@ -1,12 +1,17 @@
 package com.alurwa.berkelas.ui.subject
 
 import androidx.lifecycle.ViewModel
+import com.alurwa.data.repository.subject.SubjectRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SubjectViewModel @Inject constructor(
-
+    private val subjectRepository: SubjectRepository,
 ) : ViewModel() {
+    val observeSubject = subjectRepository.observeSubject()
+
+    fun deleteSubject(day: Int, subjectItemId: String) =
+        subjectRepository.deleteSubject(day, subjectItemId)
 
 }
