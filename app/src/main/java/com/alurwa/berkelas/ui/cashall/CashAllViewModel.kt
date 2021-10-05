f4f78183be8d99ce7ef2c919c276f56c952c74e3
Purwa Sadr <purwasadr@gmail.com>
2021-10-05 12:32:17 +0700

    Add userCashCombine and cashUserCombine variable, Add deleteCash, setUsers, setCashList method

--- app/src/main/java/com/alurwa/berkelas/ui/cashall/CashAllViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/cashall/CashAllViewModel.kt
@@ -1,6 +1,9 @@
 package com.alurwa.berkelas.ui.cashall
 
 import androidx.lifecycle.ViewModel
+import com.alurwa.berkelas.model.CashAllDateItem
+import com.alurwa.berkelas.model.CashAllPersonItem
+import com.alurwa.berkelas.util.DateTimeUtil
 import com.alurwa.common.model.Cash
 import com.alurwa.common.model.User
 import com.alurwa.data.repository.cash.CashRepository
@@ -8,6 +11,9 @@ import com.alurwa.data.repository.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,7 +29,89 @@ class CashAllViewModel @Inject constructor(
 
     val users = _users.asStateFlow()
 
-    private val _cash = MutableStateFlow<List<Cash>?>(null)
+    private val _cashList = MutableStateFlow<List<Cash>?>(null)
 
-    val cashList = _cash.asStateFlow()
+    val cashList = _cashList.asStateFlow()
+
+    val userCashCombine = users
+        .filterNotNull()
+        .combine(
+            cashList.filterNotNull()
+        ) { usersCombine, cashListCombine ->
+
+            // Membuat daftar untuk List Adapter
+            List(usersCombine.size) {
+                val user = usersCombine[it]
+
+                Timber.d("amount" + cashListCombine.size)
+
+                // Menjumlahkan properti Cash.amount jika userId di temukan di
+                // properti Cash.hasPaid
+                val amount = cashListCombine.sumOf { cash ->
+                    val isPaid = cash.hasPaid.any { hasPaid ->
+                        Timber.d("any : %s", hasPaid)
+                        hasPaid == user.uid
+                    }
+                    Timber.d("isPaid : %s", isPaid.toString())
+                    if (isPaid) 0 else -cash.amount
+                }
+
+                Timber.d("amount" + amount.toString())
+
+                CashAllPersonItem(
+                    id = user.uid, name = user.fullName, amount = amount
+                )
+            }
+        }
+
+    val cashUserCombine = cashList
+        .filterNotNull()
+        .combine(
+            users.filterNotNull()
+        ) { cashListCombine,  usersCombine ->
+
+            // Membuat daftar untuk List Adapter
+            List(cashListCombine.size) {
+                val cash = cashListCombine[it]
+
+                Timber.d("amount" + cashListCombine.size)
+
+                var count = 0
+
+                // Mengubah data dari properti date ke tanggal string
+                val dateString = DateTimeUtil.convertDateMillisToString(cash.date ?: -1)
+
+                // Menjumlahkan nilai dari properti Cash.amount jika didalam properti Cash.isPaid
+                // terdapat userId dari user
+                val amount = usersCombine.sumOf { user ->
+                    val isPaid = cash.hasPaid.any { hasPaid ->
+                        Timber.d("any : %s", hasPaid)
+                        hasPaid == user.uid
+                    }
+
+                    if (isPaid) 0 else {
+                        count++
+                        -cash.amount
+                    }
+                }
+
+                Timber.d("amount" + amount.toString())
+
+                CashAllDateItem(
+                    id = cash.cashId, date = dateString, amount = amount, count = count
+                )
+            }
+        }
+
+    fun deleteCash(cashId: String)  {
+        cashRepository.deleteCash(cashId)
+    }
+
+    fun setUsers(users: List<User>) {
+        _users.value = users
+    }
+
+    fun setCashList(cashList: List<Cash>) {
+        _cashList.value = cashList
+    }
 }
