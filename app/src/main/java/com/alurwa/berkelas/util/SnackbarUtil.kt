999a434428d08d5cd6cfdf384e298309dc0813cc
Purwa Sadr <purwasadr@gmail.com>
2021-10-06 15:57:58 +0700

    Add method for stringRes support

--- app/src/main/java/com/alurwa/berkelas/util/SnackbarUtil.kt
+++ app/src/main/java/com/alurwa/berkelas/util/SnackbarUtil.kt
@@ -1,11 +1,16 @@
 package com.alurwa.berkelas.util
 
 import android.view.View
+import androidx.annotation.StringRes
 import com.google.android.material.snackbar.Snackbar
 
 object SnackbarUtil {
     fun showShort(view: View, text: String?) {
         Snackbar.make(view, text.toString(), Snackbar.LENGTH_SHORT).show()
+    }
+
+    fun showShort(view: View, @StringRes stringRes: Int) {
+        Snackbar.make(view, view.context.getString(stringRes), Snackbar.LENGTH_SHORT).show()
     }
 }
 
