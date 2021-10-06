6cd2dc05740b548254ac8e77748c630786e0621e
Purwa Sadr <purwasadr@gmail.com>
2021-10-06 15:22:04 +0700

    Add Role enum class

---
+++ app/src/main/java/com/alurwa/berkelas/util/Role.kt
@@ -0,0 +1,30 @@
+package com.alurwa.berkelas.util
+
+import android.content.Context
+import com.alurwa.berkelas.R
+
+enum class Role(val code: String) {
+    LEADER("leader") {
+        override fun toString(context: Context) =
+            context.getString(R.string.role_co_leader)
+    },
+    CO_LEADER("co_leader") {
+        override fun toString(context: Context): String =
+            context.getString(R.string.role_co_leader)
+
+    },
+    SECRETARY("secretary") {
+        override fun toString(context: Context): String =
+            context.getString(R.string.role_secretary)
+    },
+    TREASURY("treasury") {
+        override fun toString(context: Context): String =
+            context.getString(R.string.role_treasury)
+    },
+    MEMBER("member") {
+        override fun toString(context: Context): String =
+            context.getString(R.string.role_member)
+    };
+
+    abstract fun toString(context: Context): String
+}
