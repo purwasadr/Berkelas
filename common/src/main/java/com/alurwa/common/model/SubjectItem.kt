b8359ec2d2c680d2b0107b2ffc2504b9f7e8bbc7
Purwa Sadr <purwasadr@gmail.com>
2021-10-07 11:14:23 +0700

    Add parcelable implement

--- common/src/main/java/com/alurwa/common/model/SubjectItem.kt
+++ common/src/main/java/com/alurwa/common/model/SubjectItem.kt
@@ -1,8 +1,13 @@
 package com.alurwa.common.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class SubjectItem(
+    val id: String = "",
     val subject: String = "",
     val startTime: String = "",
     val endTime: String = "",
     val teacher: String = ""
-)
+) : Parcelable
