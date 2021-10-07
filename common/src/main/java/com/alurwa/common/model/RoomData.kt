093b131015bb7bbcba07078387e7445b2590a019
Purwa Sadr <purwasadr@gmail.com>
2021-10-07 11:12:36 +0700

    Add FIELD_PASSWORD cons val

--- common/src/main/java/com/alurwa/common/model/RoomData.kt
+++ common/src/main/java/com/alurwa/common/model/RoomData.kt
@@ -9,11 +9,14 @@ data class RoomData(
     val roomName: String = "",
     val kelasName: String = "",
     val schoolName: String = "",
+    val password: String = "",
     val creatorId: String = ""
 ) : Parcelable {
     companion object {
         val EMPTY = RoomData(
             id = "", roomName = "", kelasName = "", schoolName = "", creatorId = ""
         )
+
+        const val FIELD_PASSWORD = "password"
     }
 }
