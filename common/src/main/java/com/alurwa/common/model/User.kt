5a2edaf2dbd86176a8ce36b2d209535aba795756
Purwa Sadr <purwasadr@gmail.com>
2021-10-07 11:13:46 +0700

    Add variable EMPTY and constant value

--- common/src/main/java/com/alurwa/common/model/User.kt
+++ common/src/main/java/com/alurwa/common/model/User.kt
@@ -1,10 +1,32 @@
 package com.alurwa.common.model
 
 data class User(
-    val email: String,
-    val username: String,
-    val fullName: String,
-    val nickname: String,
-    val dateOfBirth: String,
-    val gender: Int,
-)
+    val uid: String = "",
+    val email: String = "",
+    val profileImgUrl: String = "",
+    val username: String = "",
+    val fullName: String = "",
+    val nickname: String = "",
+    val roomId: String = "",
+    val dateOfBirth: Long? = null,
+    val gender: Int = 0,
+    val role: String = ""
+) {
+    companion object {
+        val EMPTY = User(
+            uid = "",
+            email = "",
+            profileImgUrl = "",
+            username = "",
+            fullName = "",
+            nickname = "",
+            roomId = "",
+            dateOfBirth = null,
+            gender = 0
+        )
+
+        const val FIELD_ROOM_ID = "roomId"
+        const val FIELD_PROFILE_IMG_URL = "profileImgUrl"
+        const val FIELD_ROLE = "role"
+    }
+}
