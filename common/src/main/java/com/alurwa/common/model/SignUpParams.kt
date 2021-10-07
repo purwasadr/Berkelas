2613804b0bb86cd6a73fb14f43c1da39518c23e1
Purwa Sadr <purwasadr@gmail.com>
2021-10-07 11:12:56 +0700

    Add default value

--- common/src/main/java/com/alurwa/common/model/SignUpParams.kt
+++ common/src/main/java/com/alurwa/common/model/SignUpParams.kt
@@ -1,11 +1,11 @@
 package com.alurwa.common.model
 
 data class SignUpParams(
-    val email: String,
-    val password: String,
-    val username: String,
-    val fullName: String,
-    val nickname: String,
-    val dateOfBirth: Long,
-    val gender: Int,
+    val email: String = "",
+    val password: String = "",
+    val username: String = "",
+    val fullName: String = "",
+    val nickname: String = "",
+    val dateOfBirth: Long? = null,
+    val gender: Int = 0,
 )
