56162fc2e50340e511279436d6e1fd6790e5f387
Purwa Sadr <purwasadr@gmail.com>
2021-10-07 11:12:07 +0700

    Add parcelable implement

--- common/src/main/java/com/alurwa/common/model/Cash.kt
+++ common/src/main/java/com/alurwa/common/model/Cash.kt
@@ -1,11 +1,15 @@
 package com.alurwa.common.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Cash(
     var cashId: String = "",
-    var date: Long = -1,
+    var date: Long? = null,
     var amount: Int = 0,
     var hasPaid: List<String> = emptyList()
-) {
+) : Parcelable {
     companion object {
         const val FIELD_CASH_ID = "cashId"
         const val FIELD_DATE = "date"
