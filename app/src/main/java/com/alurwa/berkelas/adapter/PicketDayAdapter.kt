ef290dfc94a948b0560bdaa41e5f3f0e51f2e302
Purwa Sadr <purwasadr@gmail.com>
2021-10-09 13:41:19 +0700

    Add PicketDayAdapter

---
+++ app/src/main/java/com/alurwa/berkelas/adapter/PicketDayAdapter.kt
@@ -0,0 +1,73 @@
+package com.alurwa.berkelas.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.alurwa.berkelas.databinding.ListItemPicketDayBinding
+import com.alurwa.berkelas.model.HeaderDay
+import com.alurwa.berkelas.model.PicketDayUi
+import com.alurwa.berkelas.model.PicketUi
+
+class PicketDayAdapter(
+    private val onPicketItemClick: (picket: PicketUi) -> Unit
+) : ListAdapter<PicketDayUi, PicketDayAdapter.ViewHolder>(COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            ListItemPicketDayBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    inner class ViewHolder(
+        private val binding: ListItemPicketDayBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        private val adapter = PicketAdapter() {
+            onPicketItemClick(it)
+        }
+
+        init {
+            with(binding) {
+                listPicket.layoutManager = LinearLayoutManager(binding.root.context)
+                listPicket.setHasFixedSize(true)
+                listPicket.adapter = adapter
+            }
+        }
+
+        fun bind(position: Int) {
+            val item = getItem(position)
+
+//            val picketItem = item.pickets.map {
+//                ListItem.PicketItem(
+//                    it
+//                )
+//            }
+//            val result = listOf(ListItem.Header(item.day)) + picketItem
+
+            adapter.submitList(listOf(HeaderDay(item.day)) + item.picketsUi)
+        }
+
+    }
+
+    companion object {
+
+        val COMPARATOR = object : DiffUtil.ItemCallback<PicketDayUi>() {
+            override fun areItemsTheSame(oldItem: PicketDayUi, newItem: PicketDayUi): Boolean =
+                oldItem.day == newItem.day
+
+            override fun areContentsTheSame(oldItem: PicketDayUi, newItem: PicketDayUi): Boolean =
+                oldItem == newItem
+
+        }
+    }
+}
