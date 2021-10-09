047619b27f45c3d8496dd616c2e91c5bbeef6640
Purwa Sadr <purwasadr@gmail.com>
2021-10-09 13:51:04 +0700

    Change string res

--- app/src/main/java/com/alurwa/berkelas/util/Role.kt
+++ app/src/main/java/com/alurwa/berkelas/util/Role.kt
@@ -3,10 +3,13 @@ package com.alurwa.berkelas.util
 import android.content.Context
 import com.alurwa.berkelas.R
 
+/**
+ * Berisikan Role dari aplikasi ini
+ */
 enum class Role(val code: String) {
     LEADER("leader") {
         override fun toString(context: Context) =
-            context.getString(R.string.role_co_leader)
+            context.getString(R.string.role_leader)
     },
     CO_LEADER("co_leader") {
         override fun toString(context: Context): String =
