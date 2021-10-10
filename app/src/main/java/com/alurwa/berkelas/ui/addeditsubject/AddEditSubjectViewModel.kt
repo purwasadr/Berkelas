1457f7ca82ce53c10d46be0e84b275178ea55ad3
Purwa Sadr <purwasadr@gmail.com>
2021-10-10 16:57:47 +0700

    Add startTime and endTime StateFlow and setter method

--- app/src/main/java/com/alurwa/berkelas/ui/addeditsubject/AddEditSubjectViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/addeditsubject/AddEditSubjectViewModel.kt
@@ -7,6 +7,8 @@ import com.alurwa.data.repository.subject.SubjectRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,6 +27,16 @@ class  AddEditSubjectViewModel @Inject constructor(
 
     val day = _day.asStateFlow()
 
+    private val _startTime = MutableStateFlow<String>(subjectItem?.startTime
+        ?: getNowTime()
+    )
+    val startTime = _startTime.asStateFlow()
+
+    private val _endTime = MutableStateFlow<String>(subjectItem?.startTime
+        ?: getNowTime()
+    )
+    val endTime = _endTime.asStateFlow()
+
     fun addSubject(day: Int, subject: SubjectItem) = subjectRepository.addSubject(day, subject)
 
     fun editSubject(day: Int, subject: SubjectItem) = subjectRepository.editSubject(day, subject)
@@ -32,4 +44,14 @@ class  AddEditSubjectViewModel @Inject constructor(
     fun setDay(day: Int) {
         _day.value = day
     }
+
+    fun setStartTime(time: String) {
+        _startTime.value = time
+    }
+
+    fun setEndTime(time: String) {
+        _endTime.value = time
+    }
+
+    fun getNowTime()  = SimpleDateFormat("H:mm", Locale.getDefault()).format(Date()).toString()
 }
