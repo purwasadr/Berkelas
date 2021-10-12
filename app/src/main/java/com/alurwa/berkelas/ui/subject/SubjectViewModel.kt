6cf3774667102b45d3779c124785bb2c6ae90730
Purwa Sadr <purwasadr@gmail.com>
2021-10-12 16:01:03 +0700

    Add UserRepository injection

--- app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/subject/SubjectViewModel.kt
@@ -2,16 +2,19 @@ package com.alurwa.berkelas.ui.subject
 
 import androidx.lifecycle.ViewModel
 import com.alurwa.data.repository.subject.SubjectRepository
+import com.alurwa.data.repository.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SubjectViewModel @Inject constructor(
     private val subjectRepository: SubjectRepository,
+    private val userRepository: UserRepository,
 ) : ViewModel() {
     val observeSubject = subjectRepository.observeSubject()
 
+    val role = userRepository.getUserRoomLocal()
+
     fun deleteSubject(day: Int, subjectItemId: String) =
         subjectRepository.deleteSubject(day, subjectItemId)
-
 }
