9f43771469515ceea7e5b958492616ff4388af6c
Purwa Sadr <purwasadr@gmail.com>
2021-10-14 05:41:58 +0700

    Add getRole and getUserRoom methods and add putString in saveSession method

--- data/src/main/java/com/alurwa/data/util/SessionManager.kt
+++ data/src/main/java/com/alurwa/data/util/SessionManager.kt
@@ -3,16 +3,21 @@ package com.alurwa.data.util
 import android.content.SharedPreferences
 import androidx.core.content.edit
 import com.alurwa.common.di.SessionPreferences
+import com.alurwa.common.model.UserRoom
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class SessionManager @Inject constructor(
     @SessionPreferences
     private val sharedPreferences: SharedPreferences
 ) {
 
-    fun saveUser(roomId: String) {
+    fun saveSession(roomId: String, role: String, isRoomOwner: Boolean) {
         sharedPreferences.edit(true) {
             putString(ROOM_ID, roomId)
+            putString(ROLE, role)
+            putBoolean(IS_ROOM_OWNER, isRoomOwner)
         }
     }
 
@@ -36,7 +41,20 @@ class SessionManager @Inject constructor(
         return sharedPreferences.getString(ROOM_ID, "") ?: ""
     }
 
+    fun getRole(): String {
+        return sharedPreferences.getString(ROOM_ID, "") ?: ""
+    }
+
+    fun getUserRoom() = UserRoom(
+        roomId = sharedPreferences.getString(ROOM_ID, "") ?: "",
+        role = sharedPreferences.getString(ROLE, "") ?: "",
+        isRoomOwner = sharedPreferences.getBoolean(IS_ROOM_OWNER, false)
+
+    )
+
     companion object {
         const val ROOM_ID = "room_id"
+        const val ROLE = "role"
+        const val IS_ROOM_OWNER = "is_room_owner"
     }
 }
