1790102da85cb59d297eadbcc6e26a47db920b8f
Purwa Sadr <purwasadr@gmail.com>
2021-10-13 17:26:02 +0700

    Add AttendanceDetailAdapter

---
+++ app/src/main/java/com/alurwa/berkelas/adapter/AttendanceDetailAdapter.kt
@@ -0,0 +1,53 @@
+package com.alurwa.berkelas.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.alurwa.berkelas.databinding.ListItemAttendanceDetailBinding
+import com.alurwa.berkelas.model.AttendanceDetailItem
+
+class AttendanceDetailAdapter: ListAdapter<AttendanceDetailItem, AttendanceDetailAdapter.ViewHolder>(COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            ListItemAttendanceDetailBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(
+        private val binding: ListItemAttendanceDetailBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: AttendanceDetailItem) {
+            binding.attendanceDetailItem = item
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object {
+
+        val COMPARATOR = object : DiffUtil.ItemCallback<AttendanceDetailItem>() {
+            override fun areItemsTheSame(
+                oldItem: AttendanceDetailItem,
+                newItem: AttendanceDetailItem
+            ): Boolean =
+                oldItem.userId == oldItem.userId
+
+            override fun areContentsTheSame(
+                oldItem: AttendanceDetailItem,
+                newItem: AttendanceDetailItem
+            ): Boolean =
+                oldItem == oldItem
+
+        }
+    }
+}
