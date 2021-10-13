338738b9b4e2fe3688beb351c8ba82aa5a5fded3
Purwa Sadr <purwasadr@gmail.com>
2021-10-14 05:38:24 +0700

    Add getMyRoomId method and UserRepository dependency

--- app/src/main/java/com/alurwa/berkelas/ui/roomlist/RoomListViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/roomlist/RoomListViewModel.kt
@@ -1,13 +1,20 @@
 package com.alurwa.berkelas.ui.roomlist
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.alurwa.data.repository.room.RoomRepository
+import com.alurwa.data.repository.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class RoomListViewModel @Inject constructor(
-    private val roomRepository: RoomRepository
+    private val roomRepository: RoomRepository,
+    private val userRepository: UserRepository,
+    private val stateHandle: SavedStateHandle
 ) : ViewModel() {
-    val rooms = roomRepository.getRooms()
+
+    val rooms = roomRepository.observeRooms()
+
+    fun getMyRoomId() = userRepository.getRoomIdLocal()
 }
