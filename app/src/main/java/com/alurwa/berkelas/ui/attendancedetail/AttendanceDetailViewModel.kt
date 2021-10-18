28953512983ca17cea62016bab2f7e90b8058c48
Purwa Sadr <purwasadr@gmail.com>
2021-10-18 08:52:28 +0700

    Add many things

--- app/src/main/java/com/alurwa/berkelas/ui/attendancedetail/AttendanceDetailViewModel.kt
+++ app/src/main/java/com/alurwa/berkelas/ui/attendancedetail/AttendanceDetailViewModel.kt
@@ -3,9 +3,11 @@ package com.alurwa.berkelas.ui.attendancedetail
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.alurwa.berkelas.model.AttendanceDetailItem
-import com.alurwa.berkelas.util.AttendanceCheck
+import com.alurwa.berkelas.model.AttendanceDetailUi
+import com.alurwa.berkelas.model.AttendanceHeader
 import com.alurwa.common.model.Attendance
 import com.alurwa.common.model.User
+import com.alurwa.common.util.AttendanceType
 import com.alurwa.data.repository.attendance.AttendanceRepository
 import com.alurwa.data.repository.user.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -24,6 +26,8 @@ class AttendanceDetailViewModel @Inject constructor(
 
     val date = stateHandle.get<Long>(AttendanceDetailActivity.EXTRA_DATE)!!
 
+    val userId get() = userRepository.getUserId()
+
     val observeUsersAndAttendance = userRepository.observeUsersByRoomId()
         .combine(attendanceRepository.observeAttendance(date.toString())) { usersP, attendanceP ->
             Pair(usersP, attendanceP)
@@ -35,14 +39,61 @@ class AttendanceDetailViewModel @Inject constructor(
     private val _attendance = MutableStateFlow<Attendance?>(null)
     val attendance = _attendance.asStateFlow()
 
+    private val _attendanceDetailHeader = MutableStateFlow<AttendanceHeader?>(null)
+    val attendanceDetailHeader = _attendanceDetailHeader.asStateFlow()
+
     val attendanceDetailItems =
         users.filterNotNull().combine(attendance.filterNotNull()) { usersP, attendanceP ->
-            List(usersP.size) { index ->
-                val user = usersP[index]
-                val checkCode = getCheckCode(user.uid, attendanceP.presense, attendanceP.permission, attendanceP.sick)
+            var presence = 0
+            var permit = 0
+            var sick = 0
+            var uncheck = 0
 
-                AttendanceDetailItem(userId = user.uid, name = user.fullName, check = checkCode)
+            val items = List(usersP.size) { index ->
+                val user = usersP[index]
+
+                val checkCode2 = let {
+                    attendanceP.presense.forEach {
+                        if (it == user.uid) {
+                            presence += 1
+                            return@let AttendanceType.PRESENCE.code
+                        }
+                    }
+
+                    attendanceP.permit.forEach {
+                        if (it == user.uid) {
+                            permit += 1
+                            return@let AttendanceType.PERMIT.code
+                        }
+                    }
+
+                    attendanceP.sick.forEach {
+                        if (it == user.uid) {
+                            sick += 1
+                            return@let AttendanceType.SICK.code
+                        }
+                    }
+                    uncheck += 1
+                    ""
+                }
+                val checkCode = getCheckCode(
+                    user.uid,
+                    attendanceP.presense,
+                    attendanceP.permit,
+                    attendanceP.sick
+                )
+
+                AttendanceDetailItem(userId = user.uid, name = user.fullName, check = checkCode2)
             }
+
+            AttendanceDetailUi(
+                info = AttendanceHeader(
+                    presence = presence,
+                    permit = permit,
+                    sick = sick,
+                    uncheck = uncheck
+                ), items = items
+            )
         }
 
     private fun getCheckCode(
@@ -51,19 +102,19 @@ class AttendanceDetailViewModel @Inject constructor(
     ): String {
         presence.forEach {
             if (it == userId) {
-                return AttendanceCheck.PRESENCE.code
+                return AttendanceType.PRESENCE.code
             }
         }
 
         permit.forEach {
             if (it == userId) {
-                return AttendanceCheck.PERMIT.code
+                return AttendanceType.PERMIT.code
             }
         }
 
         sick.forEach {
             if (it == userId) {
-                return AttendanceCheck.SICK.code
+                return AttendanceType.SICK.code
             }
         }
         return ""
@@ -76,4 +127,14 @@ class AttendanceDetailViewModel @Inject constructor(
     fun setUsers(items: List<User>) {
         _users.value = items
     }
+
+    fun setAttendanceDetailHeader(value: AttendanceHeader) {
+        _attendanceDetailHeader.value = value
+    }
+
+    fun setAttendance(type: String, userId: String) = attendanceRepository.addAttendanceItem(
+        attendanceId = date.toString(),
+        type = type,
+        userId = userId
+    )
 }
